db4ead80d071dabddbaa19ce378703dc365d5a72
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-03-25 09:43:45 +0100

    Modal imlplementation

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Timeline.css";
 import me from "../images/class.jpg";
-
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
+const ReadMore = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <p className="text">
+      {isReadMore ? text.slice(0, 167) : text}
+      {console.log(text.length)}
+      {console.log("text" + text)}
+      {isReadMore ? text.length > 150 : ""}
+      <span onClick={toggleReadMore} className="read-or-hide">
+        {isReadMore ? (
+          <div className="e6cont ">
+            <div className="effect6 ">
+              <FaPlus></FaPlus>
+            </div>
+          </div>
+        ) : (
+          <div className="e6cont ">
+            <div className="effect6 ">
+              <FaMinus></FaMinus>
+            </div>
+          </div>
+        )}
+        {isReadMore ? "... PLEASE CLICK +" : ""}
+      </span>
+    </p>
+  );
+};
 function timeline() {
   return (
     <div className="containerbg">
@@ -11,29 +43,51 @@ function timeline() {
         <div className="container">
           <div className="timeline">
             <div className="timeline-container">
-            <div className="e6cont ">
-                <div className="effect6 ">+</div></div>
               <div className="hovercontainer">
                 <img className="classpicwrap shadow-about" src={me} alt="me" />
               </div>
               <div className="clastextwrap">
                 <div className="timeline-bodyprim">
-                  <h4 className="timeline-title">
-                    <span className="badge">Primary</span>
-                  </h4>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Aliquam necessitatibus numquam earum ipsa fugiat veniam
-                    suscipit, officiis repudiandae, eum recusandae neque
-                    dignissimos. Cum fugit laboriosam culpa, repellendus esse
-                    commodi deserunt.
-                  </p>
-
-                  <p className="timeline-subtitle">1 Hours Ago</p>
+                  <div className="timeline-title">
+                    <span className="badge">Web Dev Instructor</span>
+                  </div>
+                  <h6>A React love affair</h6>
+                  <ReadMore>
+                    Teaching adults to become a web developer. Being their
+                    companion and motivation to obtain this highly appreciated
+                    skill. The course has been developed by myself and is
+                    React-orientated. <br/> <br/><h6>Programme</h6>{" "}
+                    <div className="programme">
+                      <ul>
+                        <li>GIT</li>
+                        <li>CSS/SAAS</li>
+                        <li>Grid/FlexBox</li>
+                        <li>Bootstrap</li> <li>Some PHP/Mysql</li>
+                        <li> Jquery</li>
+                        <li> Node</li>
+                        <li> Javascript</li>
+                        <li>React using Context</li>
+                        <li> Ui/ux design libraries (Material design)</li>
+                      </ul>
+                    </div>
+                    Having a graphice designer background myself obliges my
+                    students to have a sexy result, it is mainly a Front-End
+                    I am proud when students find a job during of soon after my course.  <br/><br/>
+                    <h6>Time-off Studies</h6>
+                    <ul className="programme">
+                    <li> Headless CMS Sanity.io.</li>
+                    <li> Coding better.</li>
+                    </ul>
+                  </ReadMore>
+                  <p className="timeline-subtitle">July 2018 - present day</p>
                 </div>
-
               </div>
             </div>
+
+
+
+
+
             <div className="timeline-container danger">
               <div className="timeline-icon">
                 <i className="far fa-grin-hearts"></i>
